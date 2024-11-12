9c2a265031dab4f5873dc287286cb02f8d96760a
RFY <876320233@qq.com>
2024-11-12 13:39:22 +0800

    完成登录注册页面和搜索界面基础框架

---
+++ vue-frontend/src/search/components/SearchInput.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref} from "vue";
+import {Search, ShoppingCart} from "@element-plus/icons-vue";
+
+const product_name = ref(null);
+</script>
+
+<template>
+  <el-input v-model="product_name" style="max-width: 50vw" placeholder="请输入商品">
+    <template #append>
+      <el-button :icon="Search"/>
+    </template>
+  </el-input>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-frontend/src/search/components/UserInfo.vue
@@ -0,0 +1,61 @@
+<script setup>
+import {ref} from "vue";
+import {ArrowDown, ArrowRight, Avatar, Star, SwitchButton, User} from "@element-plus/icons-vue";
+import {ElMessage} from "element-plus";
+import {useRouter} from "vue-router";
+
+const userInfo = ref({
+  user_name: String,
+  email: String
+})
+const testUserName = "任飞扬"
+const spaceString = ref("          ")
+</script>
+
+<template>
+  <el-dropdown class="top-right-container" trigger="hover">
+    <el-avatar
+        src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+    />
+  <template #dropdown>
+    <el-dropdown-menu>
+      <!--TODO 具体逻辑-->
+      <el-text class="mx-2" size="large" style="text-align: center;" tag="b">欢迎，{{testUserName}}</el-text>
+      <div style="margin-bottom: 10px;"></div>
+      <el-dropdown-item @click="()=>{ElMessage.success('success')}">
+        <el-icon style="margin-left:15px"><User /></el-icon>
+        <span style="margin-left:8px; margin-right: 100px">个人中心</span>
+        <el-icon><ArrowRight /></el-icon>
+      </el-dropdown-item>
+      <el-dropdown-item @click="()=>{ElMessage.success('success')}">
+        <el-icon style="margin-left:15px"><Star /></el-icon>
+        <span style="margin-left:8px; margin-right: 100px">收藏商品</span>
+        <el-icon><ArrowRight /></el-icon>
+      </el-dropdown-item>
+      <el-dropdown-item>
+        <el-icon style="margin-left:15px"><SwitchButton /></el-icon>
+        <a href="/" style="margin-left:8px; margin-right: 100px">退出登录</a>
+      </el-dropdown-item>
+    </el-dropdown-menu>
+  </template>
+  </el-dropdown>
+</template>
+
+<style scoped>
+.top-right-container {
+  position: fixed;
+  top: 4vh; /* 根据需要调整 */
+  right: 9vw; /* 根据需要调整 */
+}
+a {
+  text-decoration: none;
+  color: #606266; /* 替换为你希望的颜色 */
+}
+
+/* 移除悬停、点击和访问过状态下的下划线 */
+a:hover,
+a:active,
+a:visited {
+  text-decoration: none;
+}
+</style>
